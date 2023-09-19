a900ab78f4e91e80be9f35cdcbd966ec9702a93b
Sarabe89 <ezealeguzman@gmail.com>
2023-09-18 22:20:50 -0300

    agrego el nav

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Abrir y cerrar el menú desplegable
+    const menuButton = document.querySelector('.mobile-menu-button');
+    const dropdown = document.querySelector('.dropdown');
+    const backdrop = document.querySelector('.dropdown-backdrop');
+    const closeButtons = document.querySelectorAll('.close-button');
+
+    menuButton.addEventListener('click', function() {
+        dropdown.style.transform = 'translateX(0)';
+        backdrop.style.opacity = '1';
+        backdrop.style.pointerEvents = 'auto';
+    });
+
+    backdrop.addEventListener('click', function() {
+        dropdown.style.transform = 'translateX(-100%)';
+        backdrop.style.opacity = '0';
+        backdrop.style.pointerEvents = 'none';
+    });
+
+    closeButtons.forEach(function(closeButton) {
+        closeButton.addEventListener('click', function() {
+            dropdown.style.transform = 'translateX(-100%)';
+            backdrop.style.opacity = '0';
+            backdrop.style.pointerEvents = 'none';
+        });
+    });
+});
