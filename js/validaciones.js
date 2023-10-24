edc872f9a0e2085ae7cd38cffc6f628a479845b5
Sarabe89 <ezealeguzman@gmail.com>
2023-10-24 11:26:40 -0300

    validacion para probar

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,15 +1,49 @@
-const  formulario = document.getElementsByClassName('form');
-const inputs = document.querySelectorAll('#form input');
+const formulario = document.querySelector('.form');
+const nameInput = document.getElementById('name');
+const apellidoInput = document.getElementById('apellido');
+const phoneInput = document.getElementById('phone');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
 
+formulario.addEventListener('submit', function (event) {
+  let isValid = true;
 
-const expresiones = {
-    //usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    //password: /^.{4,12}$/, // 4 a 12 digitos.
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    phone: /^\d{7,14}$/ // 7 a 14 numeros.
-}
+  // Validación del campo de nombre
+  if (!/^[a-zA-Z\sÀ-ÿ]*$/.test(nameInput.value)) {
+    alert('Por favor, ingresa un nombre válido sin números ni caracteres especiales.');
+    isValid = false;
+  }
 
-//tendre que aprender la expresiones regulares para crear la validacion de fecha de nacimiento
-//para dni, cantidad de hijos ***(Katia: Esta info no se utiliza en un form de contacto, porfavor Dani borrar... 
-//.................................este comentario una vez sea leído)
+  // Validación del campo de apellido
+  if (!/^[a-zA-Z\sÀ-ÿ]*$/.test(apellidoInput.value)) {
+    alert('Por favor, ingresa un apellido válido sin números ni caracteres especiales.');
+    isValid = false;
+  }
+
+  // Validación del campo de teléfono
+  const phonePattern = /^\d{7,14}$/;
+  if (!phonePattern.test(phoneInput.value)) {
+    alert('Por favor, ingresa un número de teléfono válido.');
+    isValid = false;
+  }
+
+  // Validación del campo de correo electrónico
+  const emailPattern = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailPattern.test(emailInput.value)) {
+    alert('Por favor, ingresa una dirección de correo electrónico válida.');
+    isValid = false;
+  }
+
+  // Validación del campo de comentarios (para que no envie msj vacio)
+  if (messageInput.value.trim() === '') {
+    alert('Por favor, ingresa tus comentarios.');
+    isValid = false;
+  }
+
+  if (!isValid) {
+    event.preventDefault();
+  } else {
+    // Si el formulario es válido, redirige a la página de índice
+    window.location.href = 'index.html'; 
+  }
+});
